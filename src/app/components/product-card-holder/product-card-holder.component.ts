3c10f4f4f7b0abc1de9ab6b69691525edde60ed8
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-06 15:30:14 +0530

    Add ProductCardHolderComponent with template, styles, and test suite

---
+++ src/app/components/product-card-holder/product-card-holder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card-holder',
+  imports: [],
+  templateUrl: './product-card-holder.component.html',
+  styleUrl: './product-card-holder.component.css'
+})
+export class ProductCardHolderComponent {
+
+}
