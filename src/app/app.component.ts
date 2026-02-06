34d7ba5d691c37f87b14ed2fab37d7cd52348b29
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-06 15:45:30 +0530

    Add ProductCardComponent with template and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductCardComponent } from "./components/product-card/product-card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
