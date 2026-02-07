f0f3fa433219a65b0b8c2e95920238ab400d53b2
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-07 11:00:06 +0530

    Add CurrentOrderComponent with template, styles, and unit tests

---
+++ src/app/components/current-order/current-order.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-order',
+  imports: [],
+  templateUrl: './current-order.component.html',
+  styleUrl: './current-order.component.css'
+})
+export class CurrentOrderComponent {
+
+}
