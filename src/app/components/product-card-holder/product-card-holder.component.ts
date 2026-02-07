1bd43109e6616497a104ff752e3a74c14a35e929
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-07 11:36:31 +0530

    Create and add product card holder

--- src/app/components/product-card-holder/product-card-holder.component.ts
+++ src/app/components/product-card-holder/product-card-holder.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-product-card-holder',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './product-card-holder.component.html',
   styleUrl: './product-card-holder.component.css'
 })
