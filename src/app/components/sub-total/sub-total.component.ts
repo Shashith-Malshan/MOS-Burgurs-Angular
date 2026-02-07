d09d0597c189befd5a2ccc56904955b368eff549
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-07 09:54:01 +0530

    Add SubTotalComponent with template, styles, and unit tests

---
+++ src/app/components/sub-total/sub-total.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-total',
+  imports: [],
+  templateUrl: './sub-total.component.html',
+  styleUrl: './sub-total.component.css'
+})
+export class SubTotalComponent {
+
+}
