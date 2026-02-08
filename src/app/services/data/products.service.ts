b994be46dc2955e526b9447ee182a96a8cb12bc4
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-08 22:14:49 +0530

    Create orders in ProductService

--- src/app/services/data/products.service.ts
+++ src/app/services/data/products.service.ts
@@ -7,27 +7,40 @@ export class ProductsService {
 
   constructor() { }
 
-  products = [
-  {
-    name: 'Classic Deluxe',
-    price: 5.99,
-    image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
-  },
-  {
-    name: 'Double Cheese',
-    price: 5.99,
-    image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
-  },
-  {
-    name: 'Bacon Smokey',
-    price: 5.99,
-    image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
-  },
-  {
-    name: 'Garden Fresh',
-    price: 5.99,
-    image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
-  }
+  products: any[] = [
+    {
+      name: 'Classic Deluxe',
+      price: 5.99,
+      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+    },
+    {
+      name: 'Double Cheese',
+      price: 8.98,
+      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+    },
+    {
+      name: 'Bacon Smokey',
+      price: 5.03,
+      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+    },
+    {
+      name: 'Garden Fresh',
+      price: 7.99,
+      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+    }
 
-]
+  ];
+
+  orders: any[] = [
+    {
+      name: 'Bacon Smokey',
+      price: 5.03,
+      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+    },
+    {
+      name: 'Classic Deluxe',
+      price: 5.99,
+      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+    }
+  ];
 }
