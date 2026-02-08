675f98a6c3b74bccb22e54fb78d35dc092def8cf
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-08 21:59:43 +0530

    Create products in ProductService

--- src/app/services/data/products.service.ts
+++ src/app/services/data/products.service.ts
@@ -6,4 +6,28 @@ import { Injectable } from '@angular/core';
 export class ProductsService {
 
   constructor() { }
+
+  products = [
+  {
+    name: 'Classic Deluxe',
+    price: 5.99,
+    image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+  },
+  {
+    name: 'Double Cheese',
+    price: 5.99,
+    image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+  },
+  {
+    name: 'Bacon Smokey',
+    price: 5.99,
+    image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+  },
+  {
+    name: 'Garden Fresh',
+    price: 5.99,
+    image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+  }
+
+]
 }
