dd6e58a243bc93e5d3cfc8a9ddb34ee8615a14b8
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-09 15:29:56 +0530

    Add routings to filter product categories

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductCardHolderComponent } from './components/product-card-holder/product-card-holder.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"products/:category",
+    component:ProductCardHolderComponent
+    }
+];
--- src/app/services/data/products.service.ts
+++ src/app/services/data/products.service.ts
@@ -9,26 +9,80 @@ export class ProductsService {
 
   products: any[] = [
     {
-      name: 'Classic Deluxe',
-      price: 5.99,
-      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+      category: 'Beef',
+      item: [
+        {
+          name: 'Beef Deluxe',
+          price: 5.99,
+          image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+        },
+        {
+          name: 'Double Cheese',
+          price: 8.98,
+          image: 'https://unsplash.com/photos/burger-with-lettuce-and-tomato-Fn6dPYtPUMc'
+        },
+        {
+          name: 'Bacon Smokey',
+          price: 5.03,
+          image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+        },
+        {
+          name: 'Garden Fresh',
+          price: 7.99,
+          image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+        }
+      ]
     },
     {
-      name: 'Double Cheese',
-      price: 8.98,
-      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+      category: 'Chicken',
+      item: [
+        {
+          name: 'Chicken Deluxe',
+          price: 5.99,
+          image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+        },
+        {
+          name: 'Double Cheese',
+          price: 8.98,
+          image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+        },
+        {
+          name: 'Bacon Smokey',
+          price: 5.03,
+          image: 'https://unsplash.com/photos/a-delicious-cheeseburger-with-lettuce-and-tomato-SezzVhcMwDk'
+        },
+        {
+          name: 'Garden Fresh',
+          price: 7.99,
+          image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+        }
+      ]
     },
     {
-      name: 'Bacon Smokey',
-      price: 5.03,
-      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
-    },
-    {
-      name: 'Garden Fresh',
-      price: 7.99,
-      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+      category: 'Vegan',
+      item: [
+        {
+          name: 'Vegan Deluxe',
+          price: 5.99,
+          image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+        },
+        {
+          name: 'Double Cheese',
+          price: 8.98,
+          image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+        },
+        {
+          name: 'Bacon Smokey',
+          price: 5.03,
+          image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+        },
+        {
+          name: 'Garden Fresh',
+          price: 7.99,
+          image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=400&q=80'
+        }
+      ]
     }
-
   ];
 
   orders: any[] = [
@@ -44,7 +98,11 @@ export class ProductsService {
     }
   ];
 
-  getProducts(){
+  getProducts() {
+    return this.products;
+  }
+
+  getAllProducts(){
     return this.products;
   }
 }
