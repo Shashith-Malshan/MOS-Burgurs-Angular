a36b4e5bda1ba7c10be808b0a4814fcb35d570b2
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-11 23:15:03 +0530

    Implement logic to calculate sub-total and calculate tax

--- src/app/components/sub-total/sub-total.component.ts
+++ src/app/components/sub-total/sub-total.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/data/cart.service';
 
 @Component({
   selector: 'app-sub-total',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sub-total.component.html',
   styleUrl: './sub-total.component.css'
 })
-export class SubTotalComponent {
+export class SubTotalComponent  {
+  cartService =inject(CartService)
+  
+  
+  get cartSubTotal(): number {
+  return this.cartService.getsubTotal();
+}
+
+
+  
+  
+  
 
 }
