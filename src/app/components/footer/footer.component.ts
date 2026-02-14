b73f74c679b42887f38a745bc617767d9eddc24c
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-14 11:21:29 +0530

    Add date and time to footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +6,30 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  currentTime: String = ''
+
+  ngOnInit(): void {
+    this.updateClock()
+    setInterval(() => {
+      this.updateClock();
+    }, 1000); 
+
+
+  }
+
+  updateClock() {
+    const now = new Date();
+
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true
+    };
+
+    this.currentTime = now.toLocaleString('en-US', options);
+  }
 
 }
